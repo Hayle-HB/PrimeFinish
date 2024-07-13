06ba9eef2c6cf2c173eaa16d1685ebdc96af0789
Haylemeskel Haylemariam <haylemeskelhaylemariam@gmail.com>
2024-07-13 21:20:03 +0300

    New Navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react'
-import NavBar from './Component/NavBar/NavBar'
-import Home from './Component/Home/Home';
+import NavBar from './Component/NavBar/NavBar.jsx'
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
     </div>
-  );
+  )
 }
+
 export default App
